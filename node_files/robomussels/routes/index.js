8e788a3e5e29e6002d3453e2f104878a30d88c47
Tiffany Seeber <tiffany@cashswipe.com>
2015-10-27 20:45:06 -0400

    Adding extra end points for user testing

--- node_files/robomussels/routes/index.js
+++ node_files/robomussels/routes/index.js
@@ -7,25 +7,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-/* GET Hello World page. */
-router.get('/robomussels', function(req, res) {
-           res.json({"error" : false,"message" : "Hello World"});
-});
 
-/* GET Userlist page. */
-router.get('/data', function(req, res) {
-           var db = req.db;
-           var collection = db.get('test_robomussel');
-           collection.find({},{},function(e,docs){
-                           res.render('data', {
-                                      "data" : docs
-                                      });
-                           });
-           });
-
-//router.route("/users")
-//.get(function(req,res){
-router.get('/robodata', function(req,res){
+/* GET all robo data*/
+router.get('/roboall', function(req,res){
      var response = {};
      mongoOp.find({},function(err,data){
                   // Mongo command to fetch all data from collection.
@@ -37,5 +21,49 @@ router.get('/robodata', function(req,res){
                   res.json(response);
                   });
      });
+
+
+/* GET robomussel */
+router.get('/robomussel', function(req,res){
+     var response = {};
+     mongoOp.find({"biomimic":"robomussel"},function(err,data){
+                  // Mongo command to fetch all data from collection.
+                  if(err) {
+                  response = {"error" : true,"message" : "Error fetching data"};
+                  } else {
+                  response = {"error" : false,"message" : data};
+                  }
+                  res.json(response);
+                  });
+     });
+     
+/* GET robomussel- USA */
+router.get('/robomussel/USA/', function(req,res){
+     var response = {};
+     mongoOp.find({"biomimic":"robomussel","country":"USA"},function(err,data){
+                  // Mongo command to fetch all data from collection.
+                  if(err) {
+                  response = {"error" : true,"message" : "Error fetching data"};
+                  } else {
+                  response = {"error" : false,"message" : data};
+                  }
+                  res.json(response);
+                  });
+     });
+     
+/* GET robomussel- USA - Alegria */
+router.get('/robomussel/USA/Alegria', function(req,res){
+     var response = {};
+     mongoOp.find({"biomimic":"robomussel","country":"USA", "location":"Alegria"},function(err,data){
+                  // Mongo command to fetch all data from collection.
+                  if(err) {
+                  response = {"error" : true,"message" : "Error fetching data"};
+                  } else {
+                  response = {"error" : false,"message" : data};
+                  }
+                  res.json(response);
+                  });
+     });
+     
 
 module.exports = router;
